457ae0f33800a7a93b9d96e4af182f5ba11c425b
masoud hamidu <masudihmfaume@gmail.com>
2024-07-10 17:47:12 +0300

    modifying notification when data are not are not inserted

--- resources/views/motherInformation.blade.php
+++ resources/views/motherInformation.blade.php
@@ -11,8 +11,16 @@
                                     <h1>Expectant father information</h1>
                                     @if(Session::get('success'))
                                     <div class="alert alert-success">{{Session::get('success')}}</div>
-
                                     @endif
+                                    @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
                                 </div>
                             </div>
                             <br/>
